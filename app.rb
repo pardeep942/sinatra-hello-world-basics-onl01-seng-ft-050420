dde064959215ac0d7db42bd27d5d58a2fc1d06f3
PARDEEP KAUR <pkaur84@aol.com>
2020-06-14 06:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,10 @@
-reuire '../config/enviroment'
+require '../config/enviroment'
 
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   
-  
-  endconfigure do'
-  
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    
-  
-get "/" do
+  get "/" do
     erb :"Hello, World!"
 
   end
